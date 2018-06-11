2e275d5c3a12ebc345976d8b0dfe8b231dd8cfc1
Denise Thiele <thiele.denise@gmail.com>
2018-06-11 09:24:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Lets talk abou";
+var topic = "Let\´s talk about";
 
 var conversation = `${topic}`;
